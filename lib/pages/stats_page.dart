f2df716de53bf8d95b0aa1a76b5269c3106018fe
cod3r12 <85259706+cod3r12@users.noreply.github.com>
2024-03-29 15:12:57 -0500

    Start stats_page

--- lib/pages/stats_page.dart
+++ lib/pages/stats_page.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:running_log/services_and_helpers/User.dart';
+import 'package:running_log/services_and_helpers/UserDatabaseHelper.dart';
+import 'package:running_log/theme/theme.dart';
+import 'package:running_log/theme/theme_provider.dart';
+//import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
+import 'package:tinycolor2/tinycolor2.dart';
 
 class StatsPage extends StatefulWidget {
   @override
@@ -6,19 +14,79 @@ class StatsPage extends StatefulWidget {
 }
 
 class _StatsPageState extends State<StatsPage> {
+
+  Future<User> getUserFromDB () async {
+    var user = await UserDatabase.instance.getUser();
+    //UserDatabase.instance.clearDatabase();
+    if (user.isEmpty) {
+      UserDatabase.instance.addDefaultUser();
+      user = await UserDatabase.instance.getUser();
+    }
+    return user[0];
+  }  
+
   @override
   Widget build (BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        title: Text("Your stats", style: TextStyle(color: Colors.white),),
-        iconTheme: IconThemeData(color: Colors.white),
-      ),
-      body: Placeholder(),
+
+    Future<User> user = getUserFromDB();
+
+    return FutureBuilder<User>(
+      future: user,
+      builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
+        var userData = snapshot.data;
+        if (userData == null) {
+          return CircularProgressIndicator();
+        }
+        return Scaffold(
+          appBar: AppBar(
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            title: Text("Your stats", style: TextStyle(color: Colors.white),),
+            iconTheme: IconThemeData(color: Colors.white),
+          ),
+          body: Center(
+            child: SizedBox(
+              width: 300,
+              height: 300,
+              child: SfRadialGauge(
+                axes: <RadialAxis>[
+                  RadialAxis(
+                    startAngle: 270,
+                    endAngle: 270,
+                    showLabels: false,
+                    showTicks: false,
+                    axisLineStyle: AxisLineStyle(
+                      thickness: 0.05,
+                      color: Provider.of<ThemeProvider>(context).themeData == darkMode ? Color(0xff171717) : Color(0xffE8E8E8),
+                      thicknessUnit: GaugeSizeUnit.factor,
+                    ),
+                    pointers: <GaugePointer>[
+                      RangePointer(
+                        value: 50,
+                        width: 0.05,
+                        color: Theme.of(context).colorScheme.primary,
+                        sizeUnit: GaugeSizeUnit.factor,
+                        enableAnimation: true,
+                        animationDuration: 20,
+                        animationType: AnimationType.linear
+                      )
+                    ],
+                    annotations: <GaugeAnnotation>[
+                      GaugeAnnotation(
+                        positionFactor: 0.5,
+                        widget: Text(userData.goal as String),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      }
     );
   }
 }
